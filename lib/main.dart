edd1e4abbbaa622f2dca5c0464c3f5580ee27995
turkaytunc <turkaytunc00@gmail.com>
2021-01-23 16:14:28 +0300

    add: cat photo

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,62 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Card'),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey[700],
+        appBar: AppBar(
+          title: Text('Flutter Card'),
+          backgroundColor: Colors.blueGrey,
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(backgroundImage: AssetImage('assets/images/cat.jpg'),),
+              Text(
+                'Name',
+                style: TextStyle(color: Colors.redAccent),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Kent',
+                style: TextStyle(color: Colors.blue, fontSize: 30),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                'Age',
+                style: TextStyle(color: Colors.redAccent),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                '90',
+                style: TextStyle(color: Colors.blue, fontSize: 30),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.accessible_forward_outlined,
+                    color: Colors.red,
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
